d93c65d7b587b8739b271e6469a2b9243dbb191a
mayhmemo <mayhm.social@proton.me>
2026-04-22 15:44:21 -0300

    feat: make mikro-orm config

---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from '@mikro-orm/core';
+import { PostgreSqlDriver } from '@mikro-orm/postgresql';
+import 'dotenv/config';
+
+export default defineConfig({
+  driver: PostgreSqlDriver,
+  host: process.env.DATABASE_HOST || 'localhost',
+  port: parseInt(process.env.DATABASE_PORT || '5432', 10),
+  user: process.env.DATABASE_USER || 'postgres',
+  password: process.env.DATABASE_PASSWORD,
+  dbName: process.env.DATABASE_NAME || 'cappii_db',
+  entities: ['./dist/**/*.entity.js'],
+  entitiesTs: ['./src/**/*.entity.ts'],
+  debug: process.env.NODE_ENV !== 'production',
+  migrations: {
+    path: './src/migrations',
+    pathTs: './src/migrations',
+    glob: '!(*.d).{js,ts}',
+  },
+  seeder: {
+    path: './src/seeders',
+    pathTs: './src/seeders',
+    glob: '!(*.d).{js,ts}',
+  },
+});
